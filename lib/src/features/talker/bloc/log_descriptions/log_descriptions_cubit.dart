439b9f44ab72bd9204421230682f5fcf114ed442
Yelaman Yelmuratov <e99xxx@gmail.com>
2024-10-31 09:23:40 +0500

    . B fix import

--- lib/src/features/talker/bloc/log_descriptions/log_descriptions_cubit.dart
+++ lib/src/features/talker/bloc/log_descriptions/log_descriptions_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ispect/src/features/talker/core/data/models/log_description.dart';
 import 'package:ispect/src/features/talker/core/domain/ai_repository.dart';
 import 'package:meta/meta.dart';
