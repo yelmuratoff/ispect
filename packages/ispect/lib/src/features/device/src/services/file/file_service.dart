6630f8a6b564a740a1ccb56d3fa1c0b8eb312e8b
Yelaman Yelmuratov <yelamanyelmuratov@gmail.com>
2025-06-11 00:40:25 +0500

    refactor: consolidate file service logic by merging base and app file service implementations

--- packages/ispect/lib/src/features/device/src/services/file/file_service.dart
+++ packages/ispect/lib/src/features/device/src/services/file/file_service.dart
@@ -1,2 +1,65 @@
-export 'src/base_service.dart';
-export 'src/file_service.dart';
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+abstract interface class BaseFileService {
+  Future<void> deleteCacheDir();
+  Future<void> deleteAppDir();
+  Future<List<File>> getFiles();
+}
+
+class AppFileService implements BaseFileService {
+  const AppFileService._();
+
+  static const BaseFileService _service = AppFileService._();
+  static BaseFileService get instance => _service;
+
+  @override
+  Future<void> deleteAppDir() => _deleteAppDir();
+
+  Future<void> _deleteAppDir() async {
+    final appDir = await getApplicationSupportDirectory();
+
+    if (appDir.existsSync()) {
+      appDir.deleteSync(recursive: true);
+    }
+  }
+
+  @override
+  Future<void> deleteCacheDir() => _deleteCacheDir();
+
+  Future<void> _deleteCacheDir() async {
+    final cacheDir = await getTemporaryDirectory();
+
+    if (cacheDir.existsSync()) {
+      cacheDir.deleteSync(recursive: true);
+    }
+  }
+
+  @override
+  Future<List<File>> getFiles() => _getFilesList();
+
+  Future<List<File>> _getFilesList() async {
+    final fileModels = <File>[];
+    final cacheDir = await getTemporaryDirectory();
+    final files = cacheDir.listSync();
+    for (final file in files) {
+      final fileModel = File(file.path);
+      final isValid = await isValidFile(fileModel);
+      if (isValid) {
+        fileModels.add(fileModel);
+      }
+    }
+    return fileModels;
+  }
+}
+
+Future<bool> isValidFile(FileSystemEntity entity) async {
+  if (entity is! File) return false;
+  final file = entity;
+  final exists = await file.exists();
+  final isDir = await FileSystemEntity.isDirectory(file.path);
+
+  return exists && !isDir && p.extension(file.path).isNotEmpty;
+}
--- packages/ispect/lib/src/features/device/src/services/file/src/base_service.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-
-abstract interface class BaseFileService {
-  Future<void> deleteCacheDir();
-  Future<void> deleteAppDir();
-  Future<List<File>> getFiles();
-}
