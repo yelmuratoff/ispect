4cced5e0e89786771c9d8bc668cf514d7d3271ea
Yelaman Yelmuratov <yelamanyelmuratov@gmail.com>
2025-07-10 22:51:13 +0500

    refactor: Simplify log formatting by removing borders and adjusting empty message handling

--- packages/ispectify/lib/src/formatter/formatter.dart
+++ packages/ispectify/lib/src/formatter/formatter.dart
@@ -30,9 +30,7 @@ abstract interface class LoggerFormatter {
 ///
 /// Example output:
 /// ```
-/// ┌───────────────────────────────
-/// │ Log message goes here...
-/// └───────────────────────────────
+/// - Log message goes here...
 /// ```
 ///
 /// If colorization is enabled, ANSI escape codes will be applied to the output.
@@ -47,31 +45,24 @@ class ExtendedLoggerFormatter implements LoggerFormatter {
     LogDetails details,
     LoggerSettings settings,
   ) {
-    // Generate top and bottom borders using ConsoleUtils.
-    final topLine = ConsoleUtils.topLine(
-      settings.maxLineWidth,
-      lineSymbol: settings.lineSymbol,
-      withCorner: true,
-    );
-    final bottomLine = ConsoleUtils.bottomLine(
-      settings.maxLineWidth,
-      lineSymbol: settings.lineSymbol,
-      withCorner: true,
-    );
-
     // Extract the log message, ensuring it is a valid string.
     final message = details.message?.toString() ?? '';
 
     // Prepare bordered log lines. If the message is empty, show a placeholder.
-    final msgBorderedLines = message.isEmpty
-        ? ['│ (empty log message)'] // Placeholder for empty messages.
-        : message.split('\n').map((line) => '│ $line');
+    final List<String> msgBorderedLines;
+    if (message.isEmpty) {
+      msgBorderedLines = ['- (empty log message)'];
+    } else {
+      final lines = message.split('\n');
+      msgBorderedLines = [
+        '- ${lines.first}',
+        ...lines.skip(1).map((line) => '  $line'),
+      ];
+    }
 
-    // Construct the final log output with borders.
+    // Construct the final log output.
     final formattedLines = [
-      topLine,
       ...msgBorderedLines,
-      bottomLine,
     ];
 
     // Apply colorization if enabled, otherwise return the plain log.
